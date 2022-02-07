8da96f175b5de10b7ba3cd9f436ed34f3d398ac3
IAmGreyBunny <iamgreybunny@gmail.com>
2022-02-07 22:54:19 +0800

    Calendar dynamically adding date label

---
+++ src/main/java/com/example/ontrack/overview/calendar/CalendarController.java
@@ -0,0 +1,54 @@
+package com.example.ontrack.overview.calendar;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class CalendarController implements Initializable {
+
+    @FXML
+    GridPane calendarGridPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Load calendar
+        LocalDate todayDate = LocalDate.now();
+        loadCalendar(todayDate.getMonth().getValue(),todayDate.getYear());
+
+    }
+
+
+    public void loadCalendar(int month,int year)
+    {
+        LocalDate localDate = LocalDate.of(year,month,1);
+        System.out.println(localDate);
+        int firstDayOfMonth = localDate.getDayOfWeek().getValue();
+        System.out.println(firstDayOfMonth);
+
+
+        int row = 1;
+        for(int column = firstDayOfMonth; column<=7;column++)
+        {
+            System.out.println("Row:" +row);
+            System.out.println("Column:" +column);
+            Label label = new Label(localDate.toString());
+            calendarGridPane.add(label,column,row);
+            if(column==6)
+            {
+                row++;
+                column=-1;
+            }
+            if(localDate.getMonth().getValue()>month)
+            {
+                break;
+            }
+            localDate = localDate.plusDays(1);
+        }
+
+    }
+}
